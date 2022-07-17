efca18baea48d3f64c2a759377f2dda7987ed816
Andy <andy00b@gmail.com>
2022-07-16 21:20:43 -0300

    ver1.0.0

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L",
 "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c",
 "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
 "u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8",
 "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
 "{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+let passwordEl = document.getElementById("passwd1")
+let passwordE2 = document.getElementById("passwd2")
+
 function randomCar(){
     let indiceRandom = 0;
     indiceRandom = Math.floor(Math.random()*characters.length)
     return characters[indiceRandom]
 }
-
 function generarContraseña(){
     let contraseña = "";
     for(let i=0;i<16;i++){
         contraseña += randomCar();
     }
     return contraseña;
+}
+function generarContraseñas(){
+    passwordEl.textContent = generarContraseña()
+    passwordE2.textContent = generarContraseña()
 }
